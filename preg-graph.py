fb6bfc50b5f5ae9d0d2a6e2fdbbb3f6f1bfe6ee4
Alex Camilleri <camilleri.alex@gmail.com>
2024-05-26 18:54:05 +0200

    Mosaic works, but wrong size

--- preg-graph.py
+++ preg-graph.py
@@ -14,44 +14,43 @@ data_raw = pd.read_csv('graph.csv')
 data = data_raw.replace([np.inf, -np.inf], np.nan).fillna(0)
 
 x_header = 'Week'
-y_header = 'Nausea'
 
 symptoms = ['Nausea', 'Fatigue', 'Diarrhea', 'Sore Breasts', 'Breathlessness', 'Smell Sensitivity', 'Stomach Pain', 'Bad Taste in Mouth', 'Migraine', 'Heartburn', 'Knee Pain', 'Hip Pain', 'Sweating']
 
-x_data = data[x_header]
-y_data = data[y_header]
+MOSAIC_COLUMNS = 2
+num_arrays = -(-len(symptoms) // MOSAIC_COLUMNS)
+split_symptoms = [symptoms[i*MOSAIC_COLUMNS:(i+1)*MOSAIC_COLUMNS] for i in range(num_arrays)]
+last_array_length = len(split_symptoms[-1])
+desired_length = MOSAIC_COLUMNS
+if last_array_length < desired_length:
+    split_symptoms[-1] += [None] * (desired_length - last_array_length)
+print(split_symptoms)
 
-print(data.head())
-
-SPLINE_RES = 500
-spl = PchipInterpolator(data[x_header], data[y_header]) # 3 = cubic
-x_new = np.linspace(data[x_header].min(), data[x_header].max(), num=SPLINE_RES)
-y_smooth = spl(x_new)
-
-#spline = make_interp_spline(x_data,y_data)
-plt.fill_between(x_new, 5, where = x_new < 13, facecolor='#f0f0f0', alpha=0.5)
-plt.fill_between(x_new, 5, where = (x_new >= 13) & (x_new < 28), facecolor='#f0f0f0', alpha=0.3)
-plt.fill_between(x_new, 5, where = x_new > 28, facecolor='#f0f0f0', alpha=0.5)
-
-plt.plot(x_new, y_smooth, label=y_header, linestyle='--', color='orange')
-plt.scatter(x_data, y_data, s=12, color='blue', zorder=5)
-plt.xlim(left=WEEK_MIN-1, right=WEEK_MAX)
-plt.xticks(np.arange(data[x_header].min(), data[x_header].max() + 1, 4))
-#plt.plot(x_data, y_data)
-plt.xlabel('Week')
-plt.ylabel('Intensity')
-#plt.subplot_mosaic
-plt.title(y_header)
-#plt.legend()
-plt.show()
+fig, ax_dict = plt.subplot_mosaic(split_symptoms)
+fig.suptitle('Symptoms')
 
 
-#df = pd.DataFrame({'x_values': range(WEEK_MIN,WEEK_MAX), 'y_values': np.random.randn(WEEK_MAX-WEEK_MIN)})
-#plt.plot('x_values', 'y_values', data=df, color='skyblue')
-#plt.show()
-#plt.plot( 'x_values', 'y_values', data=df, color='skyblue', alpha=0.3, linewidth=3)
-#plt.show()
+for i in range(len(symptoms)):
+    if (symptoms[i] != None):
+        ax = ax_dict[symptoms[i]]
+        ax.set_title(symptoms[i])
+        x_data = data['Week']
+        y_data = data[symptoms[i]]
+        #print(y_data)
+        SPLINE_RES = 500
+        spl = PchipInterpolator(x_data, y_data)
+        x_new = np.linspace(x_data.min(), x_data.max(), num=SPLINE_RES)
+        y_smooth = spl(x_new)
+        # Draw
+        ax.fill_between(x_new, 5, where = x_new < 13, facecolor='#f0f0f0', alpha=0.5)
+        ax.fill_between(x_new, 5, where = (x_new >= 13) & (x_new < 28), facecolor='#f0f0f0', alpha=0.3)
+        ax.fill_between(x_new, 5, where = x_new > 28, facecolor='#f0f0f0', alpha=0.5)
+        ax.set_xticks(np.arange(data[x_header].min(), data[x_header].max() + 1, 4))
+        ax.set_xlabel('Week')
+        ax.set_ylabel('Intensity')
+        ax.set_xlim(left=WEEK_MIN-1, right=WEEK_MAX)
+        ax.plot(x_new, y_smooth, label=symptoms[i], linestyle='--', color='orange')
+        ax.scatter(x_data, y_data, s=6, color='blue', zorder=5)
 
-#values=np.cumsum(np.random.randn(1000,1))
-#plt.plot(values)
+plt.savefig('test.png')
 #plt.show()
